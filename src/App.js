9698aaa469ab872887cea817b88223b5064d74bf
Ladan Nazari <ladannazary@gmail.com>
2023-02-05 19:19:01 +0330

    home route added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Home from './components/Home';
 import Stocks from './components/Stocks';
 import Movies from './components/Movies';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className='main'>
       <h1>App Component</h1>
+      <Route exact path="/"> 
       <Home />
-      <Stocks />
+      </Route>
+      
       <Movies />
     </div>
   );
